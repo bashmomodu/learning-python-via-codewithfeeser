7071a683f4a2454e2c7ff29f56746a131a68f6d8
bashmomodu <bashir.momodu@gmail.com>
2026-02-13 22:41:05 +0000

    Learning to accept input

---
+++ grocery-store.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""Author: Bash
+   Learning about collecting input()"""
+
+def main():
+    """runtime code"""
+    
+   # set the store inventory
+    inventory = {"apple": 0.5, "banana": 0.5, "milk": 1.00, "bread": 3.00}
+
+
+   # welcome to our users
+    print("Welcome to our store")
+    print("Currently available is...")
+
+
+   #display what is in the store
+    for fooditem in inventory:
+          print(fooditem)
+
+
+   # grab string input from user
+
+    item_to_check = input("What price would you like to check?  ").lower()
+
+    if item_to_check in inventory:
+          print("The price of ", item_to_check, " is ", inventory.get(item_to_check))
+    else:
+          print("The store does not carry", item_to_check)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+        main()
